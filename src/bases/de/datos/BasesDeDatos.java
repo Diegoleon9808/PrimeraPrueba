f0040f73c2bba230f87a408ff7ccb53fc491ca3a
Diego <Diego@172.25.18.198>
2017-07-24 09:58:39 -0500

    Prueba Bases de Datos

---
+++ src/bases/de/datos/BasesDeDatos.java
@@ -0,0 +1,16 @@
+
+package bases.de.datos;
+
+/**
+ *
+ * @author Diego
+ */
+public class BasesDeDatos {
+
+
+    public static void main(String[] args) {
+       
+        System.out.println("Ejemplo");
+    }
+    
+}
